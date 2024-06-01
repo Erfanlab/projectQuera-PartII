9913305186594cbf74f3e8a9f631cbf82595acc2
erfanlabbafi <erfanlab2000@gmail.com>
2024-06-01 23:36:46 +0330

    add javascrit20240106

---
+++ page4/script.js
@@ -0,0 +1,184 @@
+        
+    document.addEventListener('DOMContentLoaded', (event) => {
+    const htmlElement = document.documentElement;
+    const switchElement = document.getElementById('darkModeSwitch');
+
+    // Set the default theme to dark if no setting is found in local storage
+    const currentTheme = localStorage.getItem('bsTheme') || 'dark';
+    htmlElement.setAttribute('data-bs-theme', currentTheme);
+    switchElement.checked = currentTheme === 'dark';
+
+    switchElement.addEventListener('change', function () {
+        if (this.checked) {
+            htmlElement.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('bsTheme', 'dark');
+        } else {
+            htmlElement.setAttribute('data-bs-theme', 'light');
+            localStorage.setItem('bsTheme', 'light');
+        }
+    });
+});
+
+
+////////////////ایجاد تسک جدید
+
+
+
+const actionBtn = document.querySelector("button.creat-btn");
+const userTodotitle = document.querySelector(
+    'div.todocreation input[name="todo-title"]').value;
+const userTododesc = document.querySelector(
+    'div.todocreation textarea[name="todo-desk"]').value;
+
+const selectbtnpriority= document.querySelector("button.select-btn-priority");
+const selectbtnprioritysvg= document.querySelector("button.select-btn-priority svg");
+
+const allpriority= document.querySelector("div.all-priority");
+
+selectbtnpriority.addEventListener("click", () => {
+
+    if (selectbtnprioritysvg.classList.contains("rotate-180")) {
+        selectbtnprioritysvg.classList.remove("rotate-180");
+        allpriority.style.display ="none";
+        
+    }else{
+
+        selectbtnprioritysvg.classList.add("rotate-180");
+        allpriority.style.display ="flex";
+    }
+
+   
+});
+
+
+
+
+
+
+actionBtn.addEventListener("click",creattodo);
+
+
+
+
+
+const inprogrestodos=[];
+
+function creattodo(){
+
+    const inprogrestodo = {
+        title:userTodotitle,
+        desc:userTododesc,
+    };
+
+
+
+};
+/// not add 
+/////1.09 
+
+
+
+
+
+
+
+
+
+// function getvalueuser(){
+//     const userValue = document.getElementById("recipient-name").value;
+//     if (userValue === ''){
+//         alert("خطا! هیچ عنوان تسکی انتخاب نشده است.")
+//     }else{
+//         console.log(userValue);
+//             const para = document.createElement("div");
+//             para.setAttribute("id","toodcard")
+//             para.classList.add("card","bg-body-tertiary","w-100","d-flex","flex-row","justify-content-start","align-items-center");
+//             para.style.height= "33%";
+//             document.getElementById("todolistall").appendChild(para);
+//             todolist();
+           
+        
+          
+//             // para.innerHTML=userValue;
+            
+            
+//     }
+// function todolist(){
+//     const bordercard = document.createElement("div");
+//     bordercard.classList.add("borderhigh","border","rounded-start-5","flex-row")
+//     document.getElementById("toodcard").appendChild(bordercard);
+//     const checkboxuser = document.createElement("div");
+//     checkboxuser.setAttribute('id','checkboxuser');
+//     checkboxuser.classList.add("checkboxuser");
+//     document.getElementById("toodcard").append(checkboxuser);
+//     return inputUserCheckbox()
+
+
+// function inputUserCheckbox (){
+// const inputusercheck = document.createElement("input");
+// inputusercheck.classList.add("inputUser");
+// inputusercheck.setAttribute("type","checkbox");
+// document.getElementById("checkboxuser").appendChild(inputusercheck);
+
+// }
+
+
+
+// }
+// };
+
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.innerHTML === "Hello") {
+//       x.innerHTML = "Swapped text!";
+//     } else {
+//       x.innerHTML = "Hello";
+//     }
+//   } 
+
+
+
+
+
+
+
+
+
+///////   حذف کردن تسک های انجام شده 
+function myFunctionhigh() {
+    var element = document.getElementById("textMoreHigh");
+    element.classList.toggle("text-decoration-line-through");
+    setTimeout(myFunction, 3000)
+function myFunction(){
+    alert("درخواست حذف انجام شد")
+    var removeelement = document.getElementById("cardHigh");
+    removeelement.classList.add("d-none");
+
+}
+return myFunctionhigh;
+  };
+function myFunctionmidel() {
+    var element = document.getElementById("textMoreMidel");
+    element.classList.toggle("text-decoration-line-through");
+    setTimeout(myFunction, 3000)
+    function myFunction(){
+        alert("درخواست حذف انجام شد")
+        var removeelement = document.getElementById("cardMidel");
+        removeelement.classList.add("d-none");}
+  };
+function myFunctionlower() {
+    var element = document.getElementById("textMoreLower");
+    element.classList.toggle("text-decoration-line-through");
+    setTimeout(myFunction, 3000)
+    function myFunction(){
+        alert("درخواست حذف انجام شد")
+        var removeelement = document.getElementById("cardLower");
+        removeelement.classList.add("d-none");
+    }
+        return myFunctionlower;
+  };
+
+
